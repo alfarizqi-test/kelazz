4c591a638736f1b9463505c22d0786d5b0d1c257
alfarizqi-test <alfarizqi.stura@gmail.com>
2025-12-20 08:34:56 +0700

    Refactor toggle button selector and click handler

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.querySelector("button[onclick]");
+  const toggleButton = document.querySelector("[onclick='toggleNav()']");
   const mobileNav = document.getElementById("mobileNav");
 
-  // toggle menu
   window.toggleNav = function () {
     mobileNav.classList.toggle("hidden");
   };
 
-  // klik di luar menu → tutup
   document.addEventListener("click", (e) => {
-    if (!mobileNav.contains(e.target) && !toggleButton.contains(e.target)) {
+    if (
+      !mobileNav.classList.contains("hidden") &&
+      !mobileNav.contains(e.target) &&
+      !toggleButton.contains(e.target)
+    ) {
       mobileNav.classList.add("hidden");
     }
   });
